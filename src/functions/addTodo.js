50eab9e94dc6d63a9f0a6c58aeb71959e0538616
Zhyldyz <jyldyz0610@gmail.com>
2024-06-10 21:26:48 +0200

    Path changed

--- src/functions/addTodo.js
+++ src/functions/addTodo.js
@@ -1,24 +1,5 @@
-import React, { useState } from 'react';
-import TodoItem from './TodoItem';
-import { addTodo } from '../functions/addTodo';
-const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
-  const handleAddTodo = () => {
-    const newTodo = { text: input };
-    setTodos(addTodo(todos, newTodo));
-    setInput('');
-  };
-  return (
-    <div>
-      <input value={input} onChange={(e) => setInput(e.target.value)} />
-      <button onClick={handleAddTodo}>Add Todo</button>
-      <ul>
-        {todos.map((todo, index) => (
-          <TodoItem key={index} todo={todo} />
-        ))}
-      </ul>
-    </div>
-  );
-};
-export default TodoList;
+export function addTodo(todos, newTodo) {
+    // Die Funktion gibt ein neues Array zurück, das die alten Todos und das neue Todo enthält,
+    // ohne das Original-Array zu mutieren.
+    return [...todos, newTodo];
+  }
