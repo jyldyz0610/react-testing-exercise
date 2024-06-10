5ca4281c548e1b3725e4582703afaa2b47b865ac
Zhyldyz <jyldyz0610@gmail.com>
2024-06-10 21:36:27 +0200

    Todoitem.js changed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,49 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import TodoItem from './TodoItem';
+import { addTodo } from '../functions/addTodo';
 
 const TodoList = () => {
-    const [todos, setTodos] = useState([]);
-    const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
 
-    useEffect(() => {
-        const fetchTodos = async () => {
-            try {
-                const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-                const data = await response.json();
-                const formattedTodos = data.map(todo => ({ text: todo.title, completed: todo.completed }));
-                setTodos(formattedTodos);
-            } catch (error) {
-                console.error('Error fetching todos:', error);
-            }
-        };
-
-        fetchTodos();
-    }, []);
-
-    const addTodo = () => {
-        if (input.trim() !== '') {
-            setTodos([...todos, { text: input, completed: false }]);
-            setInput('');
-        }
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const response = await fetch('/api/todos');
+      const data = await response.json();
+      setTodos(data);
     };
+    fetchTodos();
+  }, []);
 
-    return (
-        <div>
-            <label htmlFor="todoInput">Enter Todo</label>
-            <input
-                id="todoInput"
-                type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            />
-            <button onClick={addTodo}>Add Todo</button>
-            <ul>
-                {todos.map((todo, index) => (
-                    <TodoItem key={index} todo={todo} />
-                ))}
-            </ul>
-        </div>
-    );
+  const handleAddTodo = () => {
+    if (input.trim()) {
+      const newTodo = { text: input };
+      setTodos(addTodo(todos, newTodo));
+      setInput('');
+    }
+  };
+
+  return (
+    <div>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a new todo"
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <TodoItem key={index} todo={todo} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TodoList;
